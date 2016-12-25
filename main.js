f445380e213d5c8aa3ff676d8a73f299e8189f0a
tcode2k16 <alanc@macbook.local>
2016-12-25 15:20:20 +0800

    finished :)

--- main.js
+++ main.js
@@ -1,11 +1,87 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'T__T : '
+});
+
+
 const width = 3;
 
-let game = ((width) => {
+let game = ((width,rl) => {
+
     let board = [];
     for (let i = 0; i<width; i++) {
         board.push([]);
         for (let j=0; j<width; j++)
             board[i].push(0);
+    }
+
+    const obj = {
+        count: 0,
+        turn: 1,
+        board,
+        width,
+        check_win,
+        print_gird,
+        set_play,
+        start,
+        onInput
+    };
+    const welcome = 'Welcome to the TTT game. Start by typing "play x y" where x y is your location.';
+
+    
+
+
+    // rl
+    rl.on('line', onInput.bind(obj));
+    
+    function onInput(line) {
+        clearT();
+        
+        let cmd = line.split(' ');
+
+        switch (cmd[0]) {
+            case 'play':
+                switch (set_play.bind(obj)(cmd[1],cmd[2])) {
+                    case -1:
+                        console.log('invalid location');
+                        break;
+                    case -2:
+                        console.log('already taken');
+                    default:
+                        this.count++;
+                        let winner = check_win.bind(obj)();
+                        if (winner !== -1) {
+                            console.log((winner===1?"X":"O")+' is the winner!!! :)');
+                            rl.close();
+                            return;
+                        }
+                        else if (this.count >= Math.pow(width,2)) {
+                            console.log('draw >___< (try again)');
+                            rl.close();
+                            return;
+                        }
+                }
+                break;
+            default:
+                console.log('invalid command!!! :(');
+                break;
+        }
+        print_gird.bind(obj)();
+        rl.prompt();
+    }
+
+
+    function clearT() {
+        process.stdout.write('\u001B[2J\u001B[0;0f');
+    }
+
+    function start() {
+        clearT();
+        console.log("\n"+welcome);
+        this.print_gird();
+        rl.prompt();
     }
 
     function check_win() {
@@ -66,6 +142,9 @@ let game = ((width) => {
             
             process.stdout.write('\n'+divider+'\n');
         }
+        console.log("It is "+(this.turn===1 ? "X":"O")+"'s turn.");
+        console.log('count: '+this.count);
+        console.log("\n");
     }
 
     function set_play(x,y) {
@@ -76,33 +155,14 @@ let game = ((width) => {
         if (x<0 || x>width-1 || y<0 || y>width-1)
             return -1;
         
-        if (this.board[x][y] !== 0)
+        if (this.board[y][x] !== 0)
             return -2;
         
-        this.board[x][y] = this.turn;
+        this.board[y][x] = this.turn;
         this.turn = this.turn === 1 ? 2 : 1;
         return 1;
     }
+    return obj;
+})(width,rl);
 
-
-    return {
-        turn: 1,
-        board,
-        width,
-        check_win,
-        print_gird,
-        set_play
-    };
-})(width);
-
-
-// game.board = [[1,2,1,2],
-//               [0,0,2,0],
-//               [1,2,1,0],
-//               [2,2,1,0]];
-
-game.print_gird();
-game.set_play(0,0);
-game.set_play(1,1);
-game.set_play(2,2);
-game.print_gird();
+game.start();
